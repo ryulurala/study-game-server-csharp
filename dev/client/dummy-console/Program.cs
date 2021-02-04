1fc80bcfbe6ef44f0c42f7892abd9d65244f965c
ryulurala <ryulurala@gmail.com>
2021-02-04 16:01:20 +0900

    Import projects: client(dummy-console, unity), server(core, test)

---
+++ dev/client/dummy-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dummy_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
